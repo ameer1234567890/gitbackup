d6b21d8b6391cc51e29ec3108d277e0e447c9f73
Ameer Dawood <ameer1234567890@gmail.com>
2017-07-27 22:24:14 +0500

    Create gitbackup.sh

---
+++ gitbackup.sh
@@ -0,0 +1,23 @@
+github_username="ameer1234567890"
+url_start="https://github.com/"
+curl -o repos.json https://api.github.com/users/$github_username/repos
+repos="$(grep -Po '"full_name":.*?[^\\]",' repos.json | awk '{print $2}' | tr -d '"' | tr -d ',')"
+mkdir -p repos
+cd repos
+for repo in $repos; do
+  repo_name="$(echo $repo | cut -d '/' -f 2)"
+  if [ ! -d "$repo_name" ]; then
+    echo "Cloning new repository $repo_name...."
+    git clone $url_start$repo
+  else
+    echo "$repo_name already exists! Pulling any remote changes...."
+    cd $repo_name
+    git pull
+    cd ..
+  fi
+  echo "Done!"
+  echo ""
+done
+cd ..
+rm repos.json
+echo "All repositories backed up!"
