111cf5db1d898cd5c0a961b289aaded096806bde
Ameer Dawood <ameer1234567890@gmail.com>
2018-08-22 12:52:27 +0500

    Modernize

--- gitbackup.sh
+++ gitbackup.sh
@@ -1,18 +1,19 @@
+#!/bin/sh
 github_username="ameer1234567890"
 url_start="https://github.com/"
 echo "Grabbing list of repositories...."
 curl --progress-bar -o repos.json https://api.github.com/users/$github_username/repos
 repos="$(grep -Po '"full_name":.*?[^\\]",' repos.json | awk '{print $2}' | tr -d '"' | tr -d ',')"
 mkdir -p repos
-cd repos
+cd repos || exit
 for repo in $repos; do
-  repo_name="$(echo $repo | cut -d '/' -f 2)"
+  repo_name="$(echo "$repo" | cut -d '/' -f 2)"
   if [ ! -d "$repo_name" ]; then
     echo "Cloning new repository $repo_name...."
-    git clone $url_start$repo
+    git clone "$url_start$repo"
   else
     echo "$repo_name already exists! Pulling any remote changes...."
-    cd $repo_name
+    cd "$repo_name" || exit
     git pull
     cd ..
   fi
