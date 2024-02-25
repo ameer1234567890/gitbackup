16e3a24fc962d7074aefd63d0d5399e0dc63d074
Ameer Dawood <ameer1234567890@gmail.com>
2024-02-25 20:28:55 +0500

    Added extensive error checking

--- gitbackup.sh
+++ gitbackup.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 github_username="ameer1234567890"
 url_start="https://github.com/"
+exit_code_sum=0
 
 check_tools() {
   tools="curl git"
@@ -17,29 +18,31 @@ check_tools
 cd "$(dirname "$0")" || exit
 echo "Grabbing list of repositories...."
 curl --progress-bar -o repos.json https://api.github.com/users/$github_username/repos
+exit_code_sum=$((exit_code_sum + $?))
 repos="$(grep -Po '"full_name":.*?[^\\]",' repos.json | awk '{print $2}' | tr -d '"' | tr -d ',')"
 mkdir -p repos
+exit_code_sum=$((exit_code_sum + $?))
 cd repos || exit
 for repo in $repos; do
   repo_name="$(echo "$repo" | cut -d '/' -f 2)"
   if [ ! -d "$repo_name" ]; then
     echo "Cloning new repository $repo_name...."
     git clone "$url_start$repo"
-    status="$?"
+    exit_code_sum=$((exit_code_sum + $?))
   else
     echo "$repo_name already exists! Pulling any remote changes...."
     cd "$repo_name" || exit
     git pull --rebase
-    status="$?"
+    exit_code_sum=$((exit_code_sum + $?))
     cd ..
-  fi
-  if [ "$status" != 0 ]; then
-    printf "[\e[91mERROR\e[0m] Something went wrong!"
-  else
-    printf "[\e[32mINFO\e[0m] Done!"
   fi
   echo ""
 done
 cd ..
 rm repos.json
-echo "All repositories backed up!"
+exit_code_sum=$((exit_code_sum + $?))
+if [ "$exit_code_sum" != 0 ]; then
+  printf "[\e[91mERROR\e[0m] Something went wrong!"
+else
+  printf "[\e[32mINFO\e[0m] All repositories backed up!"
+fi
